3c5f70b117b76b0a7ed042eff7a3f2a3f32d094e
willstewartcode <willcstewart@gmail.com>
2024-06-05 13:15:47 -0400

    Added animations to landing page

---
+++ app.js
@@ -0,0 +1,20 @@
+/* 
+    Creates "IntersectionObserver" - detects if an element is visible
+    to the user
+*/
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+// gets elements with class "hidden"
+const hiddenElements = document.querySelectorAll('.hidden');
+
+// uses IntersectionObserver to loop through "hidden" elements
+hiddenElements.forEach((el) => observer.observe(el));
